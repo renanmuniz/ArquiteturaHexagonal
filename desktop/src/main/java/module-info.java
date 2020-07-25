a20e6878af651cc079406811e161bffe07920a2b
Renan Muniz Merlo <renan.mmerlo@gmail.com>
2020-07-25 18:02:34 -0300

    Ate aula 94

---
+++ desktop/src/main/java/module-info.java
@@ -0,0 +1,18 @@
+module conta.desktop {
+// usa conta sistema
+    requires conta.sistema;
+// usa spring
+    requires javax.inject;
+    requires spring.tx;
+    requires spring.core;
+    requires spring.beans;
+    requires spring.context;
+    requires java.sql;
+// usa javafx
+    requires javafx.controls;
+// abre telas e builds
+    opens conta.tela;
+    opens conta.dsv;
+    //opens conta.hml;
+    //opens conta.prd;
+}
